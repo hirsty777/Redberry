804c962f96966f668ed856a488db7338f04589c3
hirsty777 <777hirsty@gmail.com>
2022-07-03 11:07:17 +0400

    3

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+//select knoweladge dropdown and pass value
+function passValue(info){
+    document.querySelector('.knoweladge').value=info;
+};
+//hide show dropdown for knoweladge
+const  optionsDropdown=document.querySelector('.lvl-of-knoweladge');
+optionsDropdown.onclick=()=>{
+    optionsDropdown.classList.toggle('active');
+};
