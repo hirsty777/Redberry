1ecd6848f394dfb89908140734e77f442ec244ac
hirsty777 <777hirsty@gmail.com>
2022-07-05 12:27:42 +0400

    5

--- main.js
+++ main.js
@@ -8,17 +8,28 @@ const error=document.querySelector('.error');
 const invalid=document.getElementById('invalid');
 const correct=document.getElementById('correct');
 
+//validation icons
+let icon=document.getElementById('nameicon');
 
 
-//page 2 personafl information validation
+
+//page 2 personafl information validation==============
 nextBTN.addEventListener('click',(e)=>{
+
+    //call date of birth validation function
+    dateValidation(e);
+
+    //call phone number validation function
+    phoneValidation(e);
+
+    //call  email validation function
+    emailValidation(e); 
+
     //call  name validation function
     nameValidation(e);
     
-    //call  email validation function
-    emailValidation(e);   
-
 });
+
 
 //validate name  =====
 function nameValidation(e){
@@ -33,13 +44,24 @@ function nameValidation(e){
         11.0498 13.9286C11.0498 14.209 10.9399 14.4783 10.7436 14.6786C10.5473 14.8789 10.2804 14.9943 10 15Z" fill="#DC3545"/></svg>
         Invalid name`;
         correct.innerHTML='Name must be more then 2 letters';
+        //hide validate icon 
+        icon.style.display='none';
+        //change color to red if not valid
+        pname.style.color='red';
         e.preventDefault(); 
-        };
+    }
+    else{
+        //display validate icon 
+        icon.style.display='block';
+        //change color to black if validate
+        pname.style.color='black';
+    };
+
 };
 
 //validate email =====
 function emailValidation(e){
-    var mailformat =/^([A-Za-z0-9_\-\.])+\@redberry.ge/;
+    let mailformat =/^([A-Za-z0-9_\-\.])+\@redberry.ge/;
     if(!pemail.value.match(mailformat)){
         error.classList.add('active');
         invalid.innerHTML=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -55,6 +77,42 @@ function emailValidation(e){
     };
 
 };
+
+//validate phone number ===
+function phoneValidation(e){
+    if(pphone.value.length!==9 || pphone.value.trim()==""){
+        error.classList.add('active');
+        invalid.innerHTML=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 0C4.47768 0 0 4.47768 0 10C0 15.5223 4.47768 20 10 20C15.5223 20 20 15.5223 20 10C20 4.47768 15.5223 0
+        10 0ZM9.28571 5.17857C9.28571 5.08036 9.36607 5 9.46429 5H10.5357C10.6339 5 10.7143 5.08036 10.7143 5.17857V11.25C10.7143
+        11.3482 10.6339 11.4286 10.5357 11.4286H9.46429C9.36607 11.4286 9.28571 11.3482 9.28571 11.25V5.17857ZM10 15C9.71963 14.9943
+        9.45267 14.8789 9.25641 14.6786C9.06014 14.4783 8.95022 14.209 8.95022 13.9286C8.95022 13.6481 9.06014 13.3789 9.25641 
+        13.1786C9.45267 12.9783 9.71963 12.8629 10 12.8571C10.2804 12.8629 10.5473 12.9783 10.7436 13.1786C10.9399 13.3789 11.0498 13.6481
+        11.0498 13.9286C11.0498 14.209 10.9399 14.4783 10.7436 14.6786C10.5473 14.8789 10.2804 14.9943 10 15Z" fill="#DC3545"/></svg>
+        Invalid phone number`;
+        correct.innerHTML='Phone must be 9 digits';
+        e.preventDefault(); 
+    };
+};
+
+//validate date of birth===
+function dateValidation(e){
+    let dateval =("([0-9]{2})\/([0-9]{2})\/([0-9]{4})");
+    if(!pdate.value.match(dateval)){
+        error.classList.add('active');
+        invalid.innerHTML=`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 0C4.47768 0 0 4.47768 0 10C0 15.5223 4.47768 20 10 20C15.5223 20 20 15.5223 20 10C20 4.47768 15.5223 0
+        10 0ZM9.28571 5.17857C9.28571 5.08036 9.36607 5 9.46429 5H10.5357C10.6339 5 10.7143 5.08036 10.7143 5.17857V11.25C10.7143
+        11.3482 10.6339 11.4286 10.5357 11.4286H9.46429C9.36607 11.4286 9.28571 11.3482 9.28571 11.25V5.17857ZM10 15C9.71963 14.9943
+        9.45267 14.8789 9.25641 14.6786C9.06014 14.4783 8.95022 14.209 8.95022 13.9286C8.95022 13.6481 9.06014 13.3789 9.25641 
+        13.1786C9.45267 12.9783 9.71963 12.8629 10 12.8571C10.2804 12.8629 10.5473 12.9783 10.7436 13.1786C10.9399 13.3789 11.0498 13.6481
+        11.0498 13.9286C11.0498 14.209 10.9399 14.4783 10.7436 14.6786C10.5473 14.8789 10.2804 14.9943 10 15Z" fill="#DC3545"/></svg>
+        Invalid date`;
+        correct.innerHTML='Please enter valid date(dd/MM/yyyy)';
+        e.preventDefault(); 
+    };
+};
+
 
 
 
